a3877c6a9cc581053c33ecca530acc0d77cff22b
Terry Hearst <thearst3rd@gmail.com>
2024-11-05 00:41:17 -0500

    Handle mouse manually for inner buttons, fixes touchscreen controls

--- src/inner_button.gd
+++ src/inner_button.gd
@@ -17,6 +17,10 @@ func _ready() -> void:
 	color_sticker()
 
 
+func _process(_delta: float) -> void:
+	highlight.visible = mouse_inside()
+
+
 func flip_internal() -> void:
 	inner_index = 0 if inner_index == 1 else 1
 
@@ -25,8 +29,23 @@ func color_sticker() -> void:
 	sticker.color = colors.get_inner_color(inner_index)
 
 
+# Can I do something fancy with the area2d and not have to do this manually?
+func mouse_inside() -> bool:
+	var pos := get_local_mouse_position()
+	if pos.x < 0:
+		return false
+	if pos.x > 45.0333:
+		return false
+	var sloped_x := 26 * pos.x / 45.0333
+	if pos.y + sloped_x > 52:
+		return false
+	if pos.y - sloped_x < -52:
+		return false
+	return true
+
+
 func _input(event: InputEvent) -> void:
-	if not highlight.visible:
+	if not mouse_inside():
 		return
 
 	if event is InputEventMouseButton:
@@ -34,14 +53,6 @@ func _input(event: InputEvent) -> void:
 		if me.is_pressed() and me.button_index == MOUSE_BUTTON_LEFT:
 			_on_pressed()
 			get_viewport().set_input_as_handled()
-
-
-func _on_area_2d_mouse_entered() -> void:
-	highlight.show()
-
-
-func _on_area_2d_mouse_exited() -> void:
-	highlight.hide()
 
 
 func _on_pressed() -> void:
